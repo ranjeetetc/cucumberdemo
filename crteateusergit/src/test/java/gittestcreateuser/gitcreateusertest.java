1dab08518b0336531e74101e4ea5a90ee10bafda
LENOVO <LEN>
2020-04-19 10:49:58 +0530

    git push

---
+++ crteateusergit/src/test/java/gittestcreateuser/gitcreateusertest.java
@@ -0,0 +1,10 @@
+package gittestcreateuser;
+
+import org.testng.annotations.Test;
+
+public class gitcreateusertest {
+  @Test
+  public void tsetGitUserCreate() {
+	  System.out.println("Git demo test for create user##############################");
+  }
+}
